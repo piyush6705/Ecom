141af5dd0801d145584eb1a1c05e74867b622067
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-12 06:28:19 +0530

    added tracking page into the app

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/tracking/TrackingPage';
 import './App.css'
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart} />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   )
 }
