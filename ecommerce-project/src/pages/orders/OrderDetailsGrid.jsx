51022627ef1c8923a4b30b6a2db9da3aec6ea111
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-13 23:49:06 +0530

    make add to cart button work in order

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -1,11 +1,18 @@
-
+import axios from 'axios';
 import dayjs from 'dayjs';
 import { Fragment } from 'react';
 
-export function OrderDetailsGrid({ order }) {
+export function OrderDetailsGrid({ order,loadCart }) {
   return (
     <div className="order-details-grid">
       {order.products.map((orderProduct) => {
+        const addToCart = async() => {
+          await axios.post('/api/cart-items', {
+            productId: orderProduct.product.id,
+            quantity:1
+          });
+          await loadCart();
+        };
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
@@ -22,7 +29,7 @@ export function OrderDetailsGrid({ order }) {
               <div className="product-quantity">
                 Quantity: {orderProduct.quantity}
               </div>
-              <button className="buy-again-button button-primary">
+              <button className="buy-again-button button-primary"  onClick={addToCart} >
                 <img className="buy-again-icon" src="images/icons/buy-again.png" />
                 <span className="buy-again-message">Add to Cart</span>
               </button>
