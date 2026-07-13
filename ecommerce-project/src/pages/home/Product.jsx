035a766e6d890bc23b487356255c7d461ba815ea
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 00:11:09 +0530

    added a msg pop ups when we click add to cart option on home page

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -5,12 +5,20 @@ import axios from "axios";
 
 export function Product({product, loadCart}) {
   const [quantity, setQuantity] = useState(1);
+  const [showAddedMessage, setShowAddedMessage] = useState(false);
+
   const addToCart = async () => {
           await axios.post('/api/cart-items', {
             productId: product.id,
             quantity
           });
           await loadCart();
+
+          setShowAddedMessage(true);
+
+          setTimeout(() => {
+            setShowAddedMessage(false);
+          }, 2000);
         };
 
   const selectQuantity =  (event) => {
@@ -60,7 +68,9 @@ export function Product({product, loadCart}) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{
+        opacity: showAddedMessage ? 1: 0,
+      }}>
         <img src="images/icons/checkmark.png" />
         Added
       </div>
