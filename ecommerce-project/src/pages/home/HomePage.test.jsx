86d803bf9769148cbc63219e34c8f29531894e52
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 05:08:53 +0530

    remder hompage

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -6,3 +6,11 @@ import { MemoryRouter } from 'react-router';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { HomePage } from './HomePage';
+
+describe("HomePage component", () => {
+  let loadCart;
+  
+  it('displays the products correct' , () => {
+    render(<HomePage cart={[]} loadCart={loadCart} />);
+  });
+});
