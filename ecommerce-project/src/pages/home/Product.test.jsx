8c1775521630be115e92340870ad1515ba19ff48
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 05:04:05 +0530

    use before each to rrmove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,11 +1,17 @@
-
-import {it, expect, describe, vi} from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import { Product } from './Product';
-import {render, screen} from '@testing-library/react';
+
+vi.mock('axios');
 
 describe('Product component', () => {
-  it('displays the product details correctly', () => {
-    const product = {
+  let product;
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       image: "images/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -15,13 +21,17 @@ describe('Product component', () => {
       },
       priceCents: 1090,
       keywords: ["socks", "sports", "apparel"]
-      
     };
-    const loadCart =vi.fn();
 
-    render(<Product product={product} loadCart={loadCart}/>);
+    loadCart = vi.fn();
+  });
 
-    expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
+  it('displays the product details correctly', () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
+    expect(
+      screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
+    ).toBeInTheDocument();
 
     expect(
       screen.getByText('$10.90')
@@ -29,19 +39,31 @@ describe('Product component', () => {
 
     expect(
       screen.getByTestId('product-image')
-    ).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg')
+    ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
 
     expect(
-        screen.getByTestId('product-rating-stars-image')
-      ).toHaveAttribute(
-        'src',
-        'images/ratings/rating-45.png'
-      );
+      screen.getByTestId('product-rating-stars-image')
+    ).toHaveAttribute('src', 'images/ratings/rating-45.png');
 
     expect(
       screen.getByText('87')
     ).toBeInTheDocument();
   });
 
-  
+  it('adds a product to the cart', async () => {
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const user = userEvent.setup();
+    const addToCartButton = screen.getByTestId('add-to-cart-button');
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1
+      }
+    );
+    expect(loadCart).toHaveBeenCalled();
+  });
 });
