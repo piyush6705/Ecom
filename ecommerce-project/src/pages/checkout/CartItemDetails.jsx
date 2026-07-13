bd88e913b7928220c6402d7d7f8a8e138f1d6400
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 00:34:24 +0530

    created save optionn and make it workable update button

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,34 +1,80 @@
 import axios from 'axios';
+import { useState } from 'react';
 import { formatMoney } from '../../utils/money';
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
   };
 
+  const updateQuantity = async () => {
+    if (isUpdatingQuantity) {
+      // Save the updated quantity
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity
+      });
+
+      await loadCart();
+    }
+
+    setIsUpdatingQuantity(!isUpdatingQuantity);
+  };
+
+  const updateQuantityInput = (event) => {
+    setQuantity(Number(event.target.value));
+  };
+
   return (
     <>
-      <img className="product-image"
-        src={cartItem.product.image} />
+      <img
+        className="product-image"
+        src={cartItem.product.image}
+        alt={cartItem.product.name}
+      />
 
       <div className="cart-item-details">
         <div className="product-name">
           {cartItem.product.name}
         </div>
+
         <div className="product-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
+
         <div className="product-quantity">
           <span>
-            Quantity: <input type="text" className="quantity-textbox" />
-            <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity:&nbsp;
+
+            {isUpdatingQuantity ? (
+              <input
+                type="number"
+                min="1"
+                className="quantity-textbox"
+                value={quantity}
+                onChange={updateQuantityInput}
+              />
+            ) : (
+              <span className="quantity-label">
+                {cartItem.quantity}
+              </span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">
-            Update
+
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateQuantity}
+          >
+            {isUpdatingQuantity ? 'Save' : 'Update'}
           </span>
-          <span className="delete-quantity-link link-primary"
-            onClick={deleteCartItem}>
+
+          <span
+            className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}
+          >
             Delete
           </span>
         </div>
