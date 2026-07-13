a2fb03e3f430cbc7a047509659fb63ce3ab7ce57
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 00:05:04 +0530

    added windows.axios to use it in console too

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -6,7 +6,9 @@ import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/tracking/TrackingPage';
 import { NotFoundPage } from './pages/tracking/NotFoundPage';
-import './App.css'
+import './App.css';
+
+window.axios = axios;
 
 function App() {
   const [cart, setCart] = useState([]);
