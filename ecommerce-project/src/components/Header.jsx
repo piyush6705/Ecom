91579b747d125d2b7c7af5cdd7c3f80fc2143c69
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 00:46:34 +0530

    done with search bar now its working yaya

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate, useSearchParams } from 'react-router';
 import './header.css';
 
 export function Header({ cart }) {
-  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  // I need to use a different variable name since "search"
+  // is already being used below.
+  const searchText = searchParams.get('search');
+
+  // || '' is a shortcut. It means if searchText does not exist
+  // it will use a default value of ''.
+  const [search, setSearch] = useState(searchText || '');
 
   const updateSearchInput = (event) => {
     setSearch(event.target.value);
   };
 
   const searchProducts = () => {
-    console.log(search);
+    navigate(`/?search=${search}`);
   };
 
   let totalQuantity = 0;
