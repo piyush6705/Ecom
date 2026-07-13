c9dfc7990138366037993c9d9f15250f182d58ad
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-13 18:33:49 +0530

    done with quantity selector

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -15,7 +15,7 @@ export function CartItemDetails({ cartItem }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">2</span>
+            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
           </span>
           <span className="update-quantity-link link-primary">
             Update
