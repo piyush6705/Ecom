f2ce7f453c09785d25cd26465d9c8ed618b2c3f8
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 07:00:39 +0530

    created test foor place order button

--- ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -1,11 +1,15 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter ,useLocation} from 'react-router';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
 import { PaymentSummary } from './PaymentSummary';
 
+vi.mock('axios');
 describe('PaymentSummary component', () => {
   let paymentSummary;
   let loadCart;
+   let user;
 
   beforeEach(() => {
     paymentSummary = {
@@ -18,6 +22,7 @@ describe('PaymentSummary component', () => {
     };
 
     loadCart = vi.fn();
+    user = userEvent.setup();
   });
 
   it('displays the correct details', async () => {
@@ -34,16 +39,12 @@ describe('PaymentSummary component', () => {
       screen.getByText('Items (3):')
     ).toBeInTheDocument();
 
-    // There are multiple ways to check the text inside an element.
-    // 1. within() + getByText() + toBeInTheDocument()
     expect(
       within(screen.getByTestId('payment-summary-product-cost'))
         .getByText('$42.75')
     ).toBeInTheDocument();
 
-    // 2. getByTestId() + toHaveTextContent()
-    // (toHaveTextContent() checks the text inside an element)
-    // This solution is a little cleaner in this case.
+    
     expect(
       screen.getByTestId('payment-summary-shipping-cost')
     ).toHaveTextContent('$4.99');
@@ -59,5 +60,27 @@ describe('PaymentSummary component', () => {
     expect(
       screen.getByTestId('payment-summary-total')
     ).toHaveTextContent('$52.51');
+  });
+   it('places an order', async () => {
+    function Location() {
+      const location = useLocation();
+      return <div data-testid="url-path">{location.pathname}</div>;
+    }
+
+    render(
+      <MemoryRouter>
+        <PaymentSummary
+          paymentSummary={paymentSummary}
+          loadCart={loadCart}
+        />
+        <Location />
+      </MemoryRouter>
+    );
+    const placeOrderButton = screen.getByTestId('place-order-button');
+    await user.click(placeOrderButton);
+
+    expect(axios.post).toHaveBeenCalledWith('/api/orders');
+    expect(loadCart).toHaveBeenCalled();
+    expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
   });
 });
