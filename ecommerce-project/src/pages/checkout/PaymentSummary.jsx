1a94d0a546ecb822a7851161c60683d88c4f0b15
piyush6705 <sharmapiyush6705@gmail.com>
2026-07-14 06:57:21 +0530

    added fw test for paymentsummary

--- ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -1,21 +1,16 @@
-import { formatMoney } from '../../utils/money';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import { formatMoney } from '../../utils/money';
 
 export function PaymentSummary({ paymentSummary, loadCart }) {
   const navigate = useNavigate();
 
   const createOrder = async () => {
-  try {
-    const response = await axios.post('/api/orders');
-    console.log(response.data);
-
+    await axios.post('/api/orders');
     await loadCart();
     navigate('/orders');
-  } catch (error) {
-    console.log(error.response.data);
-  }
-};
+  };
+
   return (
     <div className="payment-summary">
       <div className="payment-summary-title">
@@ -24,43 +19,58 @@ export function PaymentSummary({ paymentSummary, loadCart }) {
 
       {paymentSummary && (
         <>
-          <div className="payment-summary-row">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-product-cost"
+          >
             <div>Items ({paymentSummary.totalItems}):</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.productCostCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-shipping-cost"
+          >
             <div>Shipping &amp; handling:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.shippingCostCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row subtotal-row">
+          <div
+            className="payment-summary-row subtotal-row"
+            data-testid="payment-summary-total-before-tax"
+          >
             <div>Total before tax:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.totalCostBeforeTaxCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row">
+          <div
+            className="payment-summary-row"
+            data-testid="payment-summary-tax"
+          >
             <div>Estimated tax (10%):</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.taxCents)}
             </div>
           </div>
 
-          <div className="payment-summary-row total-row">
+          <div
+            className="payment-summary-row total-row"
+            data-testid="payment-summary-total"
+          >
             <div>Order total:</div>
             <div className="payment-summary-money">
               {formatMoney(paymentSummary.totalCostCents)}
             </div>
           </div>
 
-          <button className="place-order-button button-primary" onClick={createOrder}
-          disabled={paymentSummary.totalItems === 0}>
+          <button className="place-order-button button-primary"
+            onClick={createOrder}>
             Place your order
           </button>
         </>
